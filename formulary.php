36a43078a4f0b26823ca99164a8393cb3e0ef9ed
Antoine Voituron <avoitu@orange.fr>
2024-01-29 12:47:40 +0100

    formulary created in HTML

---
+++ formulary.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+    <title>Exercice PHP</title>
+</head>
+<body>
+    <p>
+        <form method="post">
+        <textarea name="texte"></textarea><br>
+        <input type="radio" name="action" value="majuscules"> Majuscules<br>
+        <input type="radio" name="action" value="minuscules"> Minuscules<br>
+        <input type="radio" name="action" value="compter"> Compter les mots<br>
+        <input type="submit" value="Soumettre">
+    </p>
+</body>
+    </form>
